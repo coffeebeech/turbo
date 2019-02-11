b9c1d5ee4d6f9143a80f1c2daeb5ae575a94d61b
coffeebeech <47538612+coffeebeech@users.noreply.github.com>
2019-02-11 15:28:36 -0600

    Add files via upload

---
+++ lettergrade.py
@@ -0,0 +1,14 @@
+score = input("Enter Score: ")
+scr = float(score)
+if scr == 1 >= 0.9:
+  print("A")
+if scr < 0.9 >= 0.8:
+    print("B")
+if scr < 0.8 >= 0.7:
+    print("C")
+if scr < 0.7 >= 0.6:
+    print("D")
+if scr < 0.6:
+    print("F")
+elif scr > 1:
+    print("Error")
